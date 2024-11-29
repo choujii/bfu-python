c4de6bcd22ef03af94f6eb93b7d7375bd733bcaf
choujii <113545983+choujii@users.noreply.github.com>
2024-11-29 17:13:12 +0200

    Add files via upload

---
+++ lab9/task3/lvov_m_lab9_3.py
@@ -0,0 +1,24 @@
+import openpyxl
+from openpyxl.chart import PieChart, Reference
+
+
+path = r"C:\Users\0potter0\PycharmProjects\python_lab_0\task.xlsx"
+wb = openpyxl.load_workbook(path)
+sheet = wb.active
+
+for i in range(17, 20):
+    s = sheet.cell(row=i, column=3).value.replace(" руб.", "")
+    s = float(s)
+    sheet.cell(row=i, column=5, value=s)
+
+chart = PieChart()
+chart.title = "Распределение зарплаты по отделам"
+data_ref = Reference(sheet, min_col=5, min_row=17, max_row=19)
+
+categories_ref = Reference(sheet, min_col=2, min_row=17, max_row=19)
+
+chart.add_data(data_ref, titles_from_data=False)
+chart.set_categories(categories_ref)
+
+sheet.add_chart(chart, "L1")
+wb.save(path)
